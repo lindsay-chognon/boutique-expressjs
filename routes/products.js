e8b56052d3a1c5944292315cda0e08709dd3da77
lindsay <lindsaychognon@gmail.com>
2022-11-29 16:10:43 +0100

    getMultiple products

---
+++ routes/products.js
@@ -0,0 +1,16 @@
+// glue between the URI and the corresponding function in the services/products.js
+const express = require('express');
+const router = express.Router();
+const products = require('../services/products');
+
+/* GET programming languages. */
+router.get('/', async function(req, res, next) {
+    try {
+        res.json(await products.getMultiple(req.query.page));
+    } catch (err) {
+        console.error(`Error while getting products `, err.message);
+        next(err);
+    }
+});
+
+module.exports = router;
