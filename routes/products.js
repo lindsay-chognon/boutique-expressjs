76509113f5ea1010739b5ef693a8f1694d666273
lindsay <lindsaychognon@gmail.com>
2022-11-29 18:39:37 +0100

    POST products

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,22 @@ const express = require('express');
 const router = express.Router();
 const products = require('../services/products');
 
-/* GET programming languages. */
+/* GET products */
 router.get('/', async function(req, res, next) {
     try {
         res.json(await products.getMultiple(req.query.page));
     } catch (err) {
         console.error(`Error while getting products `, err.message);
+        next(err);
+    }
+});
+
+/* POST product */
+router.post('/', async function(req, res, next) {
+    try {
+        res.json(await products.create(req.body));
+    } catch (err) {
+        console.error(`Error while creating product`, err.message);
         next(err);
     }
 });
--- services/products.js
+++ services/products.js
@@ -2,6 +2,8 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
+
+// Get all products already paginated by 10
 async function getMultiple(page = 1){
     const offset = helper.getOffset(page, config.listPerPage);
     const rows = await db.query(
@@ -17,6 +19,25 @@ async function getMultiple(page = 1){
     }
 }
 
+// Post new product
+async function create(product){
+    const result = await db.query(
+        `INSERT INTO products 
+    (name)
+    VALUES 
+    ('${product.name}')`
+    );
+
+    let message = 'Error in creating product';
+
+    if (result.affectedRows) {
+        message = 'Product created successfully';
+    }
+
+    return {message};
+}
+
 module.exports = {
-    getMultiple
+    getMultiple,
+    create
 }
