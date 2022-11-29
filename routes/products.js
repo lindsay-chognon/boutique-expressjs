14cefc8d38060d1eb76ceb7c94cbd59cb1d52b9c
lindsay <lindsaychognon@gmail.com>
2022-11-29 19:22:51 +0100

    DELETE products

--- routes/products.js
+++ routes/products.js
@@ -33,4 +33,14 @@ router.put('/:id', async function(req, res, next) {
     }
 });
 
+/* DELETE programming language */
+router.delete('/:id', async function(req, res, next) {
+    try {
+        res.json(await products.remove(req.params.id));
+    } catch (err) {
+        console.error(`Error while deleting product`, err.message);
+        next(err);
+    }
+});
+
 module.exports = router;
