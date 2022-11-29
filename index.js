fe396bb2a10f820ddc63fab406fc09bc0ccbe4ce
lindsay <lindsaychognon@gmail.com>
2022-11-29 16:09:29 +0100

    Modification du serveur minimal avec une première route products et le parse en json

--- index.js
+++ index.js
@@ -3,6 +3,34 @@
 const express = require('express')
 const app = express()
 
-// pour pouvoir lancer le serveur
+const port = 3000;
+const productsRouter = require('./routes/products');
 
-app.listen(8080, () => {  console.log('Serveur à l\'écoute')})
+// pour parser en json
+app.use(express.json());
+
+// pour parser l'url body encodé
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({ message: "ok" });
+});
+
+// to connect with products routing
+app.use("/products", productsRouter);
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ message: err.message });
+    return;
+});
+
+// pour pouvoir lancer le serveur sur le port spécifié
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
